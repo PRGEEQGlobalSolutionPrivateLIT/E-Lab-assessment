f8def0f79a0459e12254dc0ad7660d892a53dc51
PRGEEQGlobalSolutionPrivateLIT <ranjitha.r@prgeeq.com>
2026-09-12 11:34:27 +0530

    Update page.tsx

--- apps/web/app/assessments/[assessmentId]/overview/page.tsx
+++ apps/web/app/assessments/[assessmentId]/overview/page.tsx
@@ -11,8 +11,7 @@ import {
   useSearchParams,
 } from "next/navigation";
 
-import { API_URL } from "@/src/lib/api/fetcher";
-
+import { API_URL } from "@/lib/api/fetcher";
 type Assessment = {
   id: string;
 
