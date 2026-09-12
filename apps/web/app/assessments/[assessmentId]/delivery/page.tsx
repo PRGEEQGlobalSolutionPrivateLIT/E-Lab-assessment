419896b1e80b474ed03295dddcb39fd274773486
PRGEEQGlobalSolutionPrivateLIT <ranjitha.r@prgeeq.com>
2026-09-12 11:34:04 +0530

    Update page.tsx

--- apps/web/app/assessments/[assessmentId]/delivery/page.tsx
+++ apps/web/app/assessments/[assessmentId]/delivery/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { API_URL } from "@/src/lib/api/fetcher";
+import { API_URL } from "@/lib/api/fetcher";
 
 import "./delivery.css";
 
